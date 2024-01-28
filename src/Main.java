1163edd4bce109c90570edfa6fd90c5a906ab842
Aleksandr Volik <Aleksandr_volik@mail.ru>
2024-01-28 19:03:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+        int clientAccount1 = 100;
+        int refill = 1100;
+        if (refill <= 1000) {
+            int clientAccount = clientAccount1 + refill;
+            System.out.println("Сумма на счете: " + clientAccount);
+        }
+        if (refill > 1000) {
+            int bonus = refill / 100;
+            int clientAccount = clientAccount1 + refill + bonus;
+            System.out.println("<Бонусные баллы: " + bonus);
+            System.out.println("Сумма на счете: " + clientAccount);
+        }
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
